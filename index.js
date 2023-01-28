69a37377c961d871f5985325939100f91cc5c556
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-01-28 21:03:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+
+const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+  const voicesDropdown = document.querySelector('[name="voice"]');
+  const options = document.querySelectorAll('[type="range"], [name="text"]');
+  const speakButton = document.querySelector('#speak');
+  const stopButton = document.querySelector('#stop');
+
+
+speakButton.addEventListener('click', ()=>{
+    msg.rate = options[0].value;
+    msg.pitch = options[1].value;
+    msg.text = options[2].value;
+
+speechSynthesis.speak(msg)
+})
+
+  stopButton.addEventListener('click', ()=>{
+    speechSynthesis.cancel();
+  });
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
